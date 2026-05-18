27a1aae7d3416a78952aef10ba57324b33a4a1fe
gvnstapleton12-del <gvnstapleton12@gmail.com>
2026-05-18 18:10:34 +0100

    Fix socket merge conflict

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client'
 
-<<<<<<< HEAD
 export const socket = io(
   'http://localhost:3000'
 )
-=======
-export const socket = io()
->>>>>>> cfb59899e5bfe454d337741c6688b55447ee19d1
