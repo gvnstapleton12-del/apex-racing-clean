c4ac332b3e3d3cfacb637ddeb28e2e9879566453
gvnstapleton12-del <gvnstapleton12@gmail.com>
2026-08-03 16:33:25 +0100

    chore: remove one-off scripts, analysis, backtest artifacts, orphaned deploy configs

--- find-catchall.mjs
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-const content = fs.readFileSync('./server.js', 'utf-8');
-const idx = content.indexOf("app.get('*',");
-console.log("catch-all at:", idx);
-if (idx > 0) console.log(content.slice(idx-50, idx+100));
